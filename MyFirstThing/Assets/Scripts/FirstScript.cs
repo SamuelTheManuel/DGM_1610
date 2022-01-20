3225f905a5915199f020945464e2a22f9dee19e7
Sam Calhoun <sam.r.calhoun@gmail.com>
2022-01-19 17:16:27 -0700

    created new unity project
    created new project and script

---
+++ MyFirstThing/Assets/Scripts/FirstScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;// namespaces, grabbing information from libraries
+using System.Collections.Generic;
+using UnityEngine;//these are library of commands for unity
+
+public class FirstScript : MonoBehaviour//name must match name of the file, monobehavior is a list of commands (may not be a list)
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello There, welcome to my game!!!");
+    }
+
+    // Update is called once per frame
+    void Update()//is the heart of the game
+    {
+        
+    }
+}
