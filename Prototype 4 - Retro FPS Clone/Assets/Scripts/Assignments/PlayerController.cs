4cfd317d72765ee53aa2834cf9fec4eaa0f7e971
Sam Calhoun <sam.r.calhoun@gmail.com>
2022-04-16 16:26:54 -0600

    RetreoFps player controller and shooting

--- Prototype 4 - Retro FPS Clone/Assets/Scripts/Assignments/PlayerController.cs
+++ Prototype 4 - Retro FPS Clone/Assets/Scripts/Assignments/PlayerController.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-
-    public float moveSpeed;
+    public static PlayerController instance;
+    public float moveSpeed = 10f;
     private Rigidbody rb;
     private Vector3 moveInput;
     private Vector2 mouseInput;
 
     public float mouseSensitivity = 1.0f;
+
+    public Camera viewCam;
+
+    public GameObject bulletImpact;
+    public int currentAmmo = 10;
+    private void Awake() {
+        instance = this;
+
+
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,13 +36,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        Vector3 moveH  = transform.right * moveInput.x;
+        moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); // getting input from keyboard
+        Vector3 moveH  = transform.right * moveInput.x;//used for blending movement
         Vector3 moveV = transform.forward * moveInput.z;
         rb.velocity = (moveH + moveV) * moveSpeed;
 
         mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * mouseSensitivity;
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y - mouseInput.x, transform.rotation.eulerAngles.z);
-
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+        if (Input.GetMouseButtonDown(0)) {
+            if (currentAmmo > 0) {
+                Ray ray = viewCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+                RaycastHit hit;
+                if(Physics.Raycast(ray, out hit)) {
+                    //hey you are hitting something
+                    Debug.Log("i'm looking at " + hit.transform.name);
+                    Instantiate(bulletImpact, hit.point, transform.rotation);
+                }
+                else {
+                    Debug.Log("Raycast isnt hitting anything");
+                }
+                currentAmmo --;
+            }
+        }
     }
 }
