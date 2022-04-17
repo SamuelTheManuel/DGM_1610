f68286397b01e2d232c78a7bfff65405d1681181
Sam Calhoun <sam.r.calhoun@gmail.com>
2022-04-17 14:28:28 -0600

    finished loops hw

--- Prototype 4 - Retro FPS Clone/Assets/Scripts/Assignments/Loops.cs
+++ Prototype 4 - Retro FPS Clone/Assets/Scripts/Assignments/Loops.cs
@@ -4,19 +4,60 @@ using UnityEngine;
 
 public class Loops : MonoBehaviour
 {
-    //public int numEwoks = 0;
+    //loop vars
+    public int numEwoks = 10;
+    public int numJedi = 10;
+    public int sith = 2;
+    public int ships = 100;
+    public int planets = 0;
+
+    //while vars
     
     public int ducks = 0;
+    public int swans = 0;
+    public int bears = 0;
+    public int giants = 0;
+    public int dragons = 0;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        // for(int i = 99; i > numEwoks; i--) {
-        //     Debug.Log("There are " + i + " Ewoks on a log!");
-        // }
+        for(int i = 0; i < numEwoks; i++) {
+            Debug.Log("There are " + i + " Ewoks on a log!");
+        }
+        for (int i=0; i < numJedi; i++ ){
+            Debug.Log("There are " + i + " Jedi remaining!");
+        }
+        for (int i=0; i < sith; i++ ){
+            Debug.Log("There are " + i + " Sith!");
+        }
+        for (int i=0; i < ships; i++ ){
+            Debug.Log("There are " + i + " ships remaining!");
+        }
+        for (int i=0; i < planets; i++ ){
+            Debug.Log("There are " + i + " Planets in this system");
+        }
 
         while(ducks < 10) {
             Debug.Log("There are " + ducks + " ducks in the pond!");
             ducks++;
+        }
+        while(swans < 10) {
+            Debug.Log("There are " + swans + " swans in the pond!");
+            swans++;
+        }
+        while(bears < 10) {
+            Debug.Log("There are " + bears + " bears eating swans!");
+            bears++;
+        }
+        while(giants < 10) {
+            Debug.Log("There are " + giants + " giants smacking the bears!");
+            giants++;
+        }
+        while(dragons < 10) {
+            Debug.Log("There are " + dragons + " dragons eating the giants!");
+            dragons++;
         }
     }
 
