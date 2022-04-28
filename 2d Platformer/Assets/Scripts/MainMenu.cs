09f4ec27a83f55ca12065766880de5ba0100b328
Sam Calhoun <sam.r.calhoun@gmail.com>
2022-04-28 13:55:46 -0600

    audio and main menu

---
+++ 2d Platformer/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public int sceneToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame(){
+        SceneManager.LoadScene(sceneToLoad); // scene to load
+    }
+    public void QuitGame() {
+        //application.quit only works when you build the final game
+        Debug.Log("Quit Game");
+        Application.Quit();
+
+    }
+}
